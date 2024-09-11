48813692b6366d42c6480ccf28a6d9f93b8ac01c
MYBLAN SIMEN TCHALLE <msimentc@emeal.nttdata.com>
2024-09-11 22:25:25 +0200

    aggiunta model

---
+++ demo1/demo1/src/main/java/it/corso/model/Legume.java
@@ -0,0 +1,14 @@
+package it.corso.model;
+
+public class Legume {
+
+	private String nomDuLegume;
+
+	public String getNomDuLegume() {
+		return nomDuLegume;
+	}
+
+	public void setNomDuLegume(String nomDuLegume) {
+		this.nomDuLegume = nomDuLegume;
+	}
+}
